4a273415e764b4bdeeda07cababcda3420588938
Suraj Kumar <sukr7581@gmail.com>
2025-07-02 01:44:08 +0530

    added comment for understanding in FakeStoreCategoryGateway

--- src/main/java/com/ecom/EcommerceSpring/gateway/FakeStoreCategoryGateway.java
+++ src/main/java/com/ecom/EcommerceSpring/gateway/FakeStoreCategoryGateway.java
@@ -20,10 +20,16 @@ public class FakeStoreCategoryGateway implements ICategoryGateway {
 
     @Override
     public List<CategoryDTO> getAllCategories() throws IOException  {
+
+//        1. Make the HTTP request to the FakeStore API to fetch all categories
         FakeStoreCategoryResponseDTO response = this.fakeStoreCategoryAPI.getAllFakeCategories().execute().body();
+
+//        2. Check if the response is null and throw and IOException if it is
         if(response == null){
             throw new IOException("Failed to fetch categories from Fakestore API");
         }
+
+//        3. Map the response to a list of CategoryDTO objects
         return response.getCategories().stream()
                 .map(category -> CategoryDTO.builder()
                 .name(category).build())
