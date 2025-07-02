8b3e9e68b2aaa1b365cb2f6b079afe75ebac2393
Suraj Kumar <sukr7581@gmail.com>
2025-07-02 10:38:38 +0530

    Implement baseURL in env file

--- src/main/java/com/ecom/EcommerceSpring/configuration/RetrofitConfig.java
+++ src/main/java/com/ecom/EcommerceSpring/configuration/RetrofitConfig.java
@@ -1,6 +1,7 @@
 package com.ecom.EcommerceSpring.configuration;
 
 import com.ecom.EcommerceSpring.gateway.api.FakeStoreCategoryAPI;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import retrofit2.Retrofit;
@@ -9,10 +10,19 @@ import retrofit2.converter.gson.GsonConverterFactory;
 @Configuration
 public class RetrofitConfig {
 
+    public static String getBaseUrl(){
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(  entry -> System.setProperty(entry.getKey(), entry.getValue()));
+
+        return dotenv.get("baseURL");
+
+    }
+
+
     @Bean
     public Retrofit retrofit(){
        return new Retrofit.Builder()
-               .baseUrl("https://fakestoreapi.in/api/")
+               .baseUrl(getBaseUrl())
                .addConverterFactory(GsonConverterFactory.create())
                .build();
     }
